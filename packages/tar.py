b1328498e8bac49991fd7159aed598d19f92d855
David Nielsen <gnomeuser@gmail.com>
2015-05-01 11:14:52 -0300

    Update tar to 1.28
    Update tar to 1.28 and update package syntax to use GnuXzPackage.
    
    # Upstream changelog
    https://www.gnu.org/software/tar/
    
    1.28
    
     * New checkpoint action: totals
     * Extended checkpoint format specifications
     * New option --one-top-level
     * New option --sort
     * New exclusion options
     * Refuse to read input from and write output to a tty device
     * Manpages provided
    
    1.27.1
    
     * Fix unquoting of file names obtained via the -T option
     * Fix GNU long link header timestamp (backward compatibility)
     * Fix extracting sparse members from star archives

--- packages/tar.py
+++ packages/tar.py
@@ -1,10 +1,1 @@
-class TarPackage (GnuPackage):
-	def __init__ (self):
-		GnuPackage.__init__ (self, 'tar', '1.26',
-			configure_flags = [
-				'--enable-nls=no'
-			],
-			override_properties = { 'build_dependency' : True }
-		)
-
-TarPackage ()
+GnuXzPackage ('tar', '1.28', configure_flags = ['--enable-nls=no'])
