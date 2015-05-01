856d5f43dff39bba46a0d8a9f436e750a5f289cd
David Nielsen <gnomeuser@gmail.com>
2015-05-01 11:42:14 -0300

    Update Clutter to 1.22.0
    Update Clutter to 1.22.0 and change to use GnomeXzPackage.
    
    # Upstream changelog
    https://download.gnome.org/sources/clutter/1.22/clutter-1.22.0.news
    
    Clutter 1.22.0
    
     * Improve pointer tracking in the evdev input backend
     * Do not use the events after the queue processing, to avoid dropping
    events due to compression.
     * Fix handling of multiple stages with the GDK backend
     * A regression introduced when the GDK backend implemented its own
    master clock using GdkFrameClock prevented applications with multiple
    stages to be updated properly.
    
     Updated translations
      * Kazakh, Aragonese, Indonesian, Danish, Turkish.
    
    List of bugs fixed since Clutter 1.21.8
    
     * bgo#746575 - cally-actor: Don't assume X11 backend is used when
    supported
     * bgo#746328 - evdev: Keep track of the pointer coordinate ourself

--- packages/clutter.py
+++ packages/clutter.py
@@ -1,3 +1,1 @@
-GnuXzPackage ('clutter', '1.10.6',
-	sources = [ 'http://source.clutter-project.org/sources/clutter/1.10/%{name}-%{version}.tar.xz' ],
-)
+GnomeXzPackage ('clutter', version_major = '1.22', version_minor = '0')
