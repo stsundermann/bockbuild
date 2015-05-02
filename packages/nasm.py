1882e013a92275f90a757f86cdc7c6b3da2c2319
Stephan Sundermann <stephansundermann@gmail.com>
2015-05-02 23:18:10 +0200

    Update nasm to 2.11.08
    # upstream changelog
    http://www.nasm.us/doc/nasmdocc.html
    
    C.1.1 Version 2.11.08
    
        Fix section length computation in bin backend which leaded in incorrect relocation records.
        Add a warning for numeric preprocessor definitions passed via command line which might have unexpected results otherwise.
        Add ability to specify a module name record in rdoff linker with -mn option.
        Increase label length capacity up to 256 bytes in rdoff backend for FreePascal sake, which tends to generate very long labels for procedures.
        Fix segmentation failure when rip addressing is used in macho64 backend.
        Fix access on out of memory when handling strings with a single grave. We have sixed similar problem in previous release but not all cases were covered.
        Fix NULL dereference in disassembled on BND instruction.

--- packages/nasm.py
+++ packages/nasm.py
@@ -1,7 +1,7 @@
 class NasmPackage (Package):
 	def __init__ (self):
-		Package.__init__ (self, 'nasm', '2.10.07', sources = [
-			'http://www.nasm.us/pub/nasm/releasebuilds/2.10.07/nasm-%{version}.tar.xz'
+		Package.__init__ (self, 'nasm', '2.11.08', sources = [
+			'http://www.nasm.us/pub/nasm/releasebuilds/%{version}/nasm-%{version}.tar.xz'
 		])
 
 NasmPackage ()
