102374c49da1f2384dd9a7422b9e5463f24c2d54
David Nielsen <gnomeuser@gmail.com>
2015-05-02 15:20:33 -0300

    Update gmp to 6.0.0a and enable building on both darwin-32 and darwin-64
    Update gmp to 6.0.0a and enable building on both darwin-32 and darwin-64
    
    # Upstream changelog
    https://gmplib.org/gmp6.0.html
    
    Bug fixes
    
     * The function mpz_invert now considers any number invertible in Z/1Z.
     * The mpn multiply code now handles operands of more than 231 limbs
    correctly. (Note however that the mpz code is limited to 232 bits on
    32-bit hosts and 237 bits on 64-bit hosts.)
    
    Performance enhancements
    
     * Plain division of large operands is faster and more monotonous in
    operand size.
     * Major speedup for ARM, in particular ARM Cortex-A15, thanks to
    improved assembly.
     * Major speedup for SPARC T4/T5 and speedup also for T3, thanks to a
    lot of new assembly.
     * Speedup for Intel Sandy Bridge, Ivy Bridge, Haswell, thanks to
    rewritten and vastly expanded assembly support. Speedup also for the
    older Core 2 and Nehalem.
     * Faster mixed arithmetic between mpq_class and double.
     * With g++, optimise more operations when one argument is a simple
    constant.
    
    Features
    
     * Support for new Intel and AMD CPUs.
     * Support for ARM64 alias Aarch64 alias ARMv8.
     * New public functions mpn_sec_mul and mpn_sec_sqr, implementing
    side-channel silent multiplication and squaring.
     * New public functions mpn_sec_div_qr and mpn_sec_div_r, implementing
    side-channel silent division.
     * New public functions mpn_cnd_add_n and mpn_cnd_sub_n. Side-channel
    silent conditional addition and subtraction.
     * New public function mpn_sec_powm, implementing side-channel silent
    modexp.
     * New public function mpn_sec_invert, implementing side-channel silent
    modular inversion.
     * Better support for applications which use the mpz_t type, but
    nevertheless need to call some of the lower-level mpn functions. See
    the documentation for mpz_limbs_read and related functions.

--- packages/gmp.py
+++ packages/gmp.py
@@ -1,11 +1,22 @@
 class GmpPackage (Package):
 	def __init__ (self):
-		Package.__init__ (self, 'gmp', '5.0.4', sources = [
-			'ftp://ftp.gmplib.org/pub/%{name}-%{version}/%{name}-%{version}.tar.bz2'
-		],
-		configure_flags = ['--enable-cxx --disable-dependency-tracking'])
+		Package.__init__ (self, 'gmp', '6.0.0a', sources = [
+			'https://ftp.gnu.org/gnu/%{name}/%{name}-%{version}.tar.xz' ],
+		configure_flags = ['--enable-cxx', '--disable-dependency-tracking']
+		)
+ 
+		self.source_dir_name = 'gmp-6.0.0'
 
-		if Package.profile.name == 'darwin':
-			self.configure_flags.extend (['ABI=32'])
+	def arch_build (self, arch):
+		if arch == 'darwin-32':
+				self.local_ld_flags = ['-arch i386']
+				self.local_gcc_flags = ['-arch i386']
+				self.configure_flags.extend (['ABI=32'])
+		if arch == 'darwin-64':
+				self.local_ld_flags = ['-arch x86_64']
+				self.local_gcc_flags = ['-arch x86_64']
+				self.configure_flags.extend (['ABI=64'])
+
+		Package.arch_build (self, arch, defaults = False)
 
 GmpPackage ()
