504fec0be9548a2eda3e438c119964add8fde17d
David Nielsen <gnomeuser@gmail.com>
2015-05-03 08:13:12 -0300

    Add gst-plugins-ugly 1.4.5
    Add gst-plugins-ugly 1.4.5
    
    # Upstream changelog
    http://lists.freedesktop.org/archives/gstreamer-devel/2014-December/0509
    04.html
    
     * gst/asfdemux/gstasfdemux.c:
    asfdemux: Send duration query upstream first
    An upstream element (like mms or some dlna source) might be able to
    answer the query. Try that first, else fallback to standard logic

---
+++ packages/gst-plugins-ugly.py
@@ -0,0 +1,9 @@
+GstreamerXzPackage ('gstreamer', 'gst-plugins-ugly', '1.4.5', configure_flags = [
+	' --disable-gtk-doc',
+	' --disable-asfdemux',
+	' --disable-dvdsub',
+	' --disable-dvdlpcmdec',
+	' --disable-iec958',
+	' --disable-mpegstream'
+	' --disable-realmedia'
+])
