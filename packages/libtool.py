6fc6cf099644c1402b82cf0a261493893d055d08
David Nielsen <gnomeuser@gmail.com>
2015-05-06 10:05:06 -0300

    Update libtool to 2.4.6
    Update libtool to 2.4.6
    
    # Conflicts:
    #	packages/libtool.py

--- packages/libtool.py
+++ packages/libtool.py
@@ -1,13 +1,12 @@
 class LibtoolPackage (GnuPackage):
         def __init__ (self):
-                GnuPackage.__init__ (self, 'libtool', '2.4.2', override_properties = { 'build_dependency' : True })
+                GnuPackage.__init__ (self, 'libtool', '2.4.6', override_properties = { 'build_dependency' : True })
 
         def install (self):
 		Package.install (self)
 		self.sh ('rm -f "%{prefix}/bin/glibtool"')
-                self.sh ('ln -s "%{prefix}/bin/libtool"  "%{prefix}/bin/glibtool"')
-                self.sh ('rm -f "%{prefix}/bin/glibtoolize"')
-                self.sh ('ln -s "%{prefix}/bin/libtoolize"  "%{prefix}/bin/glibtoolize"')
-
+		self.sh ('ln -s "%{prefix}/bin/libtool"  "%{prefix}/bin/glibtool"')
+		self.sh ('rm -f "%{prefix}/bin/glibtoolize"')
+		self.sh ('ln -s "%{prefix}/bin/libtoolize"  "%{prefix}/bin/glibtoolize"')
 
 LibtoolPackage ()
