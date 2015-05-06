c9f350a651982f954ddc9d1b264986d04833e24c
David Nielsen <gnomeuser@gmail.com>
2015-05-06 09:56:39 -0300

    Update glib to 2.44.0
    Update glib to 2.44.0
    
    # Conflicts:
    #	packages/glib.py

--- packages/glib.py
+++ packages/glib.py
@@ -2,60 +2,49 @@ class GlibPackage (GnomeXzPackage):
 	def __init__ (self):
 		GnomeXzPackage.__init__ (self,
 			'glib',
-			version_major = '2.36',
-			version_minor = '4')
+			version_major = '2.44',
+			version_minor = '0')
 
 		self.darwin = Package.profile.name == 'darwin'
 
 		if Package.profile.name == 'darwin':
-			#link to specific revisions for glib 2.30.x
-			self.sources.extend ([
-				# https://trac.macports.org/export/91680/trunk/dports/devel/glib2/files/config.h.ed
-				'patches/glib/config.h.ed',
-				# https://trac.macports.org/export/98985/trunk/dports/devel/glib2/files/patch-configure.diff
-				'patches/glib/patch-configure.diff',
-				# https://trac.macports.org/export/42728/trunk/dports/devel/glib2/files/patch-gi18n.h.diff
-				'patches/glib/patch-gi18n.h.diff',
-				# https://trac.macports.org/export/92608/trunk/dports/devel/glib2/files/patch-gio_gdbusprivate.c.diff
-				'patches/glib/patch-gio_gdbusprivate.c.diff',
-				# https://trac.macports.org/export/49466/trunk/dports/devel/glib2/files/patch-gio_xdgmime_xdgmime.c.diff
-				'patches/glib/patch-gio_xdgmime_xdgmime.c.diff',
-				# https://trac.macports.org/export/91680/trunk/dports/devel/glib2/files/patch-glib-2.0.pc.in.diff
-				'patches/glib/patch-glib-2.0.pc.in.diff',
-				# https://trac.macports.org/export/64476/trunk/dports/devel/glib2/files/patch-glib_gunicollate.c.diff
-				'patches/glib/patch-glib_gunicollate.c.diff',
+			
+			# 05.05.15: Patches from MacPorts to compile glib 2.44.0 on OS X, excluding patches which redefine paths for MacPorts' build system
+			# See: https://trac.macports.org/browser/trunk/dports/devel/glib2/Portfile
 
-				# Bug 6156 - [gtk] Quitting the application with unsaved file and answering Cancel results in crash
-				# https://bugzilla.xamarin.com/attachment.cgi?id=2214
-				'patches/glib-recursive-poll.patch',
+			self.sources.extend ([
+				'https://trac.macports.org/export/135842/trunk/dports/devel/glib2/files/config.h.ed',
+				'https://trac.macports.org/export/135842/trunk/dports/devel/glib2/files/patch-configure.diff',
+				'https://trac.macports.org/export/135842/trunk/dports/devel/glib2/files/patch-glib-2.0.pc.in.diff',
+				'https://trac.macports.org/export/135842/trunk/dports/devel/glib2/files/patch-glib-gmain.c.diff',
+				'https://trac.macports.org/export/135842/trunk/dports/devel/glib2/files/patch-glib_gunicollate.c.diff',
+				'https://trac.macports.org/export/135842/trunk/dports/devel/glib2/files/patch-gi18n.h.diff',
+				'https://trac.macports.org/export/135842/trunk/dports/devel/glib2/files/patch-get-launchd-dbus-session-address.diff',
+				'https://trac.macports.org/export/135860/trunk/dports/devel/glib2/files/patch-gio-giotypes.h.diff',
+				'https://trac.macports.org/export/135842/trunk/dports/devel/glib2/files/patch-gmodule-gmodule-dl.c.diff'
 			])
 
 	def prep (self):
 		Package.prep (self)
 		if self.darwin:
-			for p in range (2, 8):
-				self.sh ('patch -p0 < %{sources[' + str (p) + ']}')
-			for p in range (8, len (self.sources)):
-				self.sh ('patch --ignore-whitespace -p1 < %{sources[' + str (p) + ']}')
+			for p in range (2, len (self.sources)):
+				self.sh ('patch --ignore-whitespace -p0 < %{sources[' + str (p) + ']}')
 
 	def arch_build (self, arch):
 
 		if arch == 'darwin-fat': #multi-arch  build pass
-			self.local_ld_flags = ['-arch i386' , '-arch x86_64']
-			self.local_gcc_flags = ['-arch i386' , '-arch x86_64', '-Os']
-			self.local_configure_flags = ['--disable-dependency-tracking']
+			self.local_ld_flags = ['-arch i386' , '-arch x86_64', '-lresolv', '-bind_at_load']
+			self.local_gcc_flags = ['-arch i386' , '-arch x86_64', '-Os', '-fstrict-aliasing']
 		elif arch == 'darwin-32':
-				self.local_ld_flags = ['-arch i386']
-				self.local_gcc_flags = ['-arch i386']
-				self.local_configure_flags = ['--disable-dependency-tracking']
+				self.local_ld_flags = ['-arch i386', '-lresolv', '-bind_at_load']
+				self.local_gcc_flags = ['-arch i386', '-fstrict-aliasing']
 		elif arch == 'darwin-64':
-				self.local_ld_flags = ['-arch x86_64']
-				self.local_gcc_flags = ['-arch x86_64']
-				self.local_configure_flags = ['--disable-dependency-tracking']
+				self.local_ld_flags = ['-arch x86_64', '-lresolv', '-bind_at_load']
+				self.local_gcc_flags = ['-arch x86_64', '-fstrict-aliasing']
 
 		#modified build for darwin
 		if arch.startswith ('darwin'): 
-			self.local_configure_flags.extend (['--disable-compile-warnings'])
+			self.local_configure_flags.extend (['--disable-compile-warnings', '--enable-static', '--disable-libelf', '--disable-dependency-tracking', '--disable-dtrace'])
 			Package.configure (self)
 			self.sh (
 				# 'autoconf',
