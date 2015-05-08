80008c4a8793ed0eeed8c29e44ca239c39c6d503
David Nielsen <gnomeuser@gmail.com>
2015-05-08 11:49:57 -0300

    Update pixman to 0.32.6
    Update pixman to 0.32.6
    
    # Upstream changelog
    http://lists.freedesktop.org/archives/pixman/2014-July/003291.html
    
    0.32.6:
     * MIPS: Fix exported symbols in public API.
     * configure.ac: Check if the compiler supports GCC vector extensions
     * create_bits(): Cast the result of height * stride to size_t

--- packages/pixman.py
+++ packages/pixman.py
@@ -1,6 +1,6 @@
 class PixmanPackage (CairoGraphicsPackage):
 	def __init__ (self):
-		CairoGraphicsPackage.__init__ (self, 'pixman', '0.30.0')
+		CairoGraphicsPackage.__init__ (self, 'pixman', '0.32.6')
 		
 		#This package would like to be built with fat binaries
 		if Package.profile.m64 == True:
