9d1e5cc392903d21ca0486693d4cea23c77984f1
David Nielsen <gnomeuser@gmail.com>
2015-05-11 10:58:45 -0300

    Update libsoup to 2.50.0
    Update libsoup to 2.50.0 and change package to use GnomeXzPackage
    
    # Upstream changelog
    https://mail.gnome.org/archives/ftp-release-list/2015-March/msg00261.htm
    l
    
    2.50.0
    
    Mark as stable
    
    2.49.92
    
     * Fixed an NTLM problem that caused spurious "Authentication Failed"
    errors in evolution-ews. [bgo#735197]
    
    2.49.91
    
     * WebSockets! Libsoup now has support for WebSockets in both
    SoupSession and SoupServer. [bgo#627738, Dan Winship and Lionel
    Landwerlin, based on code from the Cockpit project]
     * Added support for NTLMv2 Session Security to SoupAuthNTLM. (This is
    transparent to the app/user; if the server supports it, it will be
    used.) [bgo#739192, Adam Seering]
     * Added soup_server_accept_iostream() to allow feeding a
    non-GSocketConnection GIOStream to SoupServer (eg, when accepting HTTP
    requests on some other sort of connection). [bgo#744186, Marc-André
    Lureau]
     * Added soup_session_steal_connection() and
    soup_client_context_steal_connection(), to allow you to steal a
    GIOStream from a SoupSession or SoupServer and then use it for non-HTTP
    purposes. [bgo#721343, Dan Winship]
     * Added soup_server_add_early_handler(), for adding SoupServer
    handlers to run at :got-headers time rather than :got-body (eg, to set
    up a streaming read of the request body). [bgo#625645, Dan Winship]
     * Added a new SOUP_MESSAGE_IGNORE_CONNECTION_LIMITS flag, which can be
    used to bypass SoupSession:max-conns and SoupSession:max-conns-per-host
    on a per-message basis. [bgo#744720, Carlos Garcia Campos]
     * SoupCache: Leaked resources are now cleaned up when opening the
    cache. [bgo#667682]
     * SoupCache: The "Age" header is no longer added to cached responses.
    [bgo#741108]
     * SoupCache: Fixed the interpretation of the "must-revalidate"
    directive to not be stricter than required. [bgo#744468]
     * SoupCache: Messages retrieved from cache now behave slightly more
    like non-cache messages in terms of SoupSession signals and timing.
    [bgo#744788] Additionally, there is a new SoupMessage::starting signal,
    which is like SoupSession::request-started, but emitted for cached
    resources as well. [bgo#731153]
     * Fixed soup_session_cancel_message() to work reliably on paused
    messages. [bgo#745094, Sven Neumann]
     * Added code-coverage support to the build. [bgo#744744, Marc-André
    Lureau]
     * Fixed the library symbol check in "make check" to work when building
    with linker options that result in slightly different organization.
    [bgo#741348, Iain Lane]
     * Added a hack to SoupSessionFeature so that people will stop
    re-reporting an old Eclipse bug against libsoup. [bgo#710242]
     * Miscellaneous bug and leak fixes in the test programs, leak fixes in
    libsoup, and documentation updates.

--- packages/libsoup.py
+++ packages/libsoup.py
@@ -1,9 +1,24 @@
-class LibsoupPackage (GnomePackage):
+class LibsoupPackage (GnomeXzPackage):
 	def __init__ (self):
-		GnomePackage.__init__ (self, 'libsoup', '2.33', '90')
+		GnomeXzPackage.__init__ (self, 'libsoup', '2.50', '0')
 		self.configure_flags = [
 			'--disable-gtk-doc',
 			'--without-gnome'
 		]
 
+	def arch_build (self, arch):
+		if arch == 'darwin-32':
+				self.local_ld_flags = ['-arch i386']
+				self.local_gcc_flags = ['-arch i386']
+				self.local_configure_flags.extend (['--build=i386-apple-darwin11.2.0'])
+				# The following will only work with bash according to:
+				# http://www.gossamer-threads.com/lists/python/python/30602
+				os.environ['VERSIONER_PYTHON_PREFER_32_BIT'] = 'yes'
+		elif arch == 'darwin-64':
+				self.local_ld_flags = ['-arch x86_64']
+				self.local_gcc_flags = ['-arch x86_64']
+				self.local_configure_flags.extend (['--build=x86_64-apple-darwin11.2.0'])
+
+		Package.arch_build (self, arch, defaults = False)
+		
 LibsoupPackage ()
