9a57b04bf1f670db058e7b856b4cd5a91dc0dc22
David Nielsen <gnomeuser@gmail.com>
2015-05-12 14:21:14 -0300

    Update wavpack to 4.70.0
    Update wavpack to 4.70.0

--- packages/wavpack.py
+++ packages/wavpack.py
@@ -1,3 +1,18 @@
-Package ('wavpack', '4.60.1', sources = [
-	'http://www.wavpack.com/%{name}-%{version}.tar.bz2'
-])
+class WavpackPackage (Package):
+	def __init__ (self):
+		Package.__init__ (self, 'wavpack', '4.70.0', sources = [
+			'http://www.wavpack.com/%{name}-%{version}.tar.bz2'
+		]) 
+
+		self.sources.extend ([
+				# define @exec_prefix@ in wavpack.pc to fix configuration
+				'https://trac.macports.org/export/136232/trunk/dports/audio/wavpack/files/patch-wavpack.pc.in.diff'
+				])
+
+	def prep (self):
+		Package.prep (self)
+		if Package.profile.name == 'darwin':
+			for p in range (1, len (self.sources)):
+				self.sh ('patch -p0 --ignore-whitespace < "%{sources[' + str (p) + ']}"')
+
+WavpackPackage ()
