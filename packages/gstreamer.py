f27b9730ad2e38cd0a8010e7882d9e475e4a3e08
David Nielsen <gnomeuser@gmail.com>
2015-05-12 22:03:27 -0300

    Fix GStreamer packages use of prefix

--- packages/gstreamer.py
+++ packages/gstreamer.py
@@ -1,4 +1,4 @@
 GstreamerXzPackage (project  = 'gstreamer', name = 'gstreamer', version = '1.4.5', configure_flags = [
         '--disable-gtk-doc', 
-        '--prefix="%{prefix}'
+        '--prefix=%{prefix}'
 ])
