41bf0bdf3cdb3917aa20f04825cc6142b4540fa4
David Nielsen <gnomeuser@gmail.com>
2015-05-13 18:12:41 -0300

    Update taglib-sharp to 2.1.0.1
    Update taglib-sharp to 2.1.0.1. This is git master.

--- packages/taglib-sharp.py
+++ packages/taglib-sharp.py
@@ -1,4 +1,15 @@
-Package ('taglib-sharp', '2.0.4.0',
-	sources = [ 'http://download.banshee.fm/%{name}/%{version}/%{name}-%{version}.tar.gz' ],
-	configure_flags = [ '--disable-docs' ]
-)
+class TaglibSharpPackage (GitHubPackage):
+	def __init__ (self):		
+		GitHubPackage.__init__ (self, 'mono', 'taglib-sharp', '2.1.0.1',
+			revision = 'a5f6949a53d09ce63ee7495580d6802921a21f14',
+			configure_flags = ['--disable-docs'])
+
+	def arch_build (self, arch):
+
+		# Use mcs rather than gmcs
+		self.sh ('sed -ie "s:MCS, gmcs:MCS, mcs:g" "configure.ac"')
+
+		self.sh ('./autogen.sh --disable-docs --prefix="%{prefix}"')
+		Package.arch_build (self, arch, defaults = False)
+
+TaglibSharpPackage ()
