40e42371b202f6becf9924b8dd091489d8610976
David Nielsen <gnomeuser@gmail.com>
2015-05-12 21:47:22 -0300

    Update taglib to 1.9.1.99
    This is a git package since taglib 1.9.1 was released October 8, 2013
    and there has been quite a lot of activity in git since then.

--- packages/taglib.py
+++ packages/taglib.py
@@ -1,3 +1,17 @@
-Package ('taglib', '1.6.3', sources = [
-	'http://developer.kde.org/~wheeler/files/src/%{name}-%{version}.tar.gz'
-])
+class TaglibPackage (GitHubPackage):
+	def __init__ (self):		
+		GitHubPackage.__init__ (self, 'taglib', 'taglib', '1.9.1.99',
+			revision = '62ab41fa07bfe6456eafc7ffacbfa2043cba0668')
+
+	def build (self):
+		self.sh (
+			# Let's set a bunch of variables, to make cmake feel loved
+			'CMAKE_PREFIX_PATH=%{prefix}',
+			'CMAKE_LIBRARY_PATH=%{prefix}/lib',
+			'CMAKE_INSTALL_PREFIX=%{prefix}',
+
+			# build and install
+			'cmake -DCMAKE_INSTALL_PREFIX=%{prefix} . && make all install',
+			)
+ 
+TaglibPackage ()
