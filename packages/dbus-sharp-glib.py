12a8c9e4fede74ee1853b133264bb7d640922934
David Nielsen <gnomeuser@gmail.com>
2015-05-13 18:07:28 -0300

    Update dbus-sharp(-glib) to 0.8.99 and 0.5.0.1 (breaks API)
    Update dbus-sharp and dbus-sharp-glib to 0.8.99 and 0.5.0.1
    respectively (git master).
    
    Note this changes update carries with it an API change and all
    applications using dbus-sharp(-glib) must be updated.

--- packages/dbus-sharp-glib.py
+++ packages/dbus-sharp-glib.py
@@ -1,3 +1,11 @@
-Package ('dbus-sharp-glib', '0.5.0', sources = [
-	'https://github.com/downloads/mono/dbus-sharp/%{name}-%{version}.tar.gz'
-])
+class DbusSharpGlibPackage (GitHubPackage):
+	def __init__ (self):		
+		GitHubPackage.__init__ (self, 'mono', 'dbus-sharp-glib', '0.5.0.1',
+			revision = 'eeccd9126d050c0537be64530a933adefe2ee5a6')
+
+	def arch_build (self, arch):
+		self.sh ('sed -ie "s:GMCS, gmcs, no:GMCS, mcs, no:g" "configure.ac"')
+		self.sh ('./autogen.sh --prefix="%{prefix}"')
+		Package.arch_build (self, arch, defaults = False)
+ 
+DbusSharpGlibPackage ()
--- packages/dbus-sharp.py
+++ packages/dbus-sharp.py
@@ -1,3 +1,14 @@
-Package ('dbus-sharp', '0.7.0', sources = [
-	'https://github.com/downloads/mono/%{name}/%{name}-%{version}.tar.gz'
-])
+class DbusSharpPackage (GitHubPackage):
+	def __init__ (self):		
+		GitHubPackage.__init__ (self, 'mono', 'dbus-sharp', '0.8.99',
+			revision = 'fb3dd1221fb5dceb4c07d7747f638d940680e63f')
+
+	def arch_build (self, arch):
+
+		# Use mcs rather than gmcs
+		self.sh ('sed -ie "s:GMCS, gmcs, no:GMCS, mcs, no:g" "configure.ac"')
+		
+		self.sh ('./autogen.sh --prefix="%{prefix}"')
+		Package.arch_build (self, arch, defaults = False)
+
+DbusSharpPackage ()
