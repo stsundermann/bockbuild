44de9d52cccda496c15fcab89235d0e5f75fd2df
David Nielsen <gnomeuser@gmail.com>
2015-05-13 12:40:19 -0300

    Fix zlib compilation
    If —enable-debug is passed to configure, zlib fails on configure. Also
    add a patch from Macports to fix a configure issue with clang.

--- packages/zlib.py
+++ packages/zlib.py
@@ -1,3 +1,22 @@
-Package ('zlib', '1.2.8',
-	sources = [ 'http://zlib.net/%{name}-%{version}.tar.gz' ]
-)
+class ZlibPackage (Package):
+	def __init__ (self):		
+		Package.__init__ (self, 'zlib', '1.2.8', 
+			sources = ['http://zlib.net/%{name}-%{version}.tar.gz'])
+
+		self.sources.extend ([
+		# Fix configuration when using clang
+		'https://trac.macports.org/export/136252/trunk/dports/archivers/zlib/files/patch-configure-clang.diff'
+		])
+		
+	def prep (self):
+		Package.prep (self)
+		for p in range (1, len (self.sources)):
+			self.sh ('patch -p1 --ignore-whitespace < "%{sources[' + str (p) + ']}"')
+
+	def arch_build (self, arch):
+		# zlib fails on configure if --enable-debug is present in configure_flags
+		self.configure_flags.remove("--enable-debug")
+
+		Package.arch_build (self, arch, defaults = False)
+
+ZlibPackage ()
