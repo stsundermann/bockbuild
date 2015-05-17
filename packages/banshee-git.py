80104741b03de72b5b2d07c3337f4a7ab0da9a22
David Nielsen <gnomeuser@gmail.com>
2015-05-17 13:09:06 -0300

    Update banshee-git
    Update banshee-git to most recent git master and update package to use
    GnomeGitPackage.

--- packages/banshee-git.py
+++ packages/banshee-git.py
@@ -1,18 +1,3 @@
-class BansheePackage (Package):
-	def __init__ (self):
-		Package.__init__ (self, 'banshee', 'master')
-
-		self.sources = [
-			'git://git.gnome.org/banshee'
-		]
-		self.git_branch = 'master'
-
-		self.configure = [ 'NOCONFIGURE=1 ./autogen.sh && ./profile-configure %{profile.name} --prefix=%{prefix}' ]
-		self.sources.extend([
-		])
-
-	def prep (self):
-		Package.prep (self)
-		#self.sh ('patch -p1 < %{sources[1]}')
-
-BansheePackage ()
+GnomeGitPackage ('banshee', '2.99', 
+	revision = '5638d304a328f92b516e0543994504b2140be8de',
+	override_properties = { 'configure': 'NOCONFIGURE=1 ./autogen.sh && ./profile-configure %{profile.name} --prefix=%{prefix}'})
