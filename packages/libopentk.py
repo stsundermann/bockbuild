dfdec1a15520e8ac768d6d9d095647a8035a9ed2
David Nielsen <gnomeuser@gmail.com>
2015-05-23 19:30:03 -0300

    Update libopentk to 1.1.4.1
    Update libopentk to 1.1.4.1.
    
    This is in fact a snapshot from git master post 1.1.4c

--- packages/libopentk.py
+++ packages/libopentk.py
@@ -1,5 +1,17 @@
-Package ('opentk', '2010-10-06',
-	sources = [ 'http://downloads.sourceforge.net/project/opentk/opentk/opentk-1.0/2010-10-06/opentk-2010-10-06.zip' ],
-	source_dir_name = '%{name}',
-	configure_flags = [ '' ]
-)
+class OpenTKPackage (GitHubPackage):
+	def __init__ (self):		
+		GitHubPackage.__init__ (self, 'opentk', 'opentk', '1.1.4.1',
+			revision = '788b039e325b23575d6e8ed652a802446d1ea06e',
+			override_properties = {
+			'configure': 'echo empty',
+			'make': 'xbuild  /p:Configuration=Release OpenTK.sln'}
+			)
+
+	def install (self):
+		self.sh (
+			'mkdir -p %{prefix}/lib/mono/opentk/',
+			'cp Source/OpenTK/obj/Release/OpenTK.dll %{prefix}/lib/mono/opentk/',
+			'cp Source/OpenTK/obj/Release/OpenTK.OpenTK.dll.config %{prefix}/lib/mono/opentk/',
+			'gacutil -i %{prefix}/lib/mono/opentk/OpenTK.dll -package OpenTK')
+
+OpenTKPackage ()
