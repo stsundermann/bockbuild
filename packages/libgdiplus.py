daa3a01e047d58fc611222af4cef98e7fc297f5f
David Nielsen <gnomeuser@gmail.com>
2015-05-23 19:28:56 -0300

    Update libgdiplus to 3.12
    Update libgdiplus to 3.12

--- packages/libgdiplus.py
+++ packages/libgdiplus.py
@@ -1,1 +1,4 @@
-GitHubTarballPackage('mono', 'libgdiplus', '2.11', '4e7ab0f555a13a6b2f954c714c4ee5213954ff79', configure = 'CFLAGS="%{gcc_flags} %{local_gcc_flags} -I/opt/X11/include" ./autogen.sh --prefix="%{package_prefix}"', override_properties = { 'make': 'C_INCLUDE_PATH="" make' })
+GitHubPackage ('mono', 'libgdiplus', '3.12',
+	revision = 'f18514c8babf20301f8bbd1f0cfd587ed48a480b',
+	configure = 'CFLAGS="%{gcc_flags} %{local_gcc_flags} -I/opt/X11/include" ./autogen.sh --prefix="%{package_prefix}"', 
+	override_properties = { 'make': 'C_INCLUDE_PATH="" make' })
