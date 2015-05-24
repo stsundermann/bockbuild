7dfcb51d8ae01542e10e03499df0097f9e37d00a
Stephan Sundermann <stephansundermann@gmail.com>
2015-05-24 15:19:11 +0200

    Fix soundtouch

--- packages/soundtouch.py
+++ packages/soundtouch.py
@@ -14,12 +14,12 @@ class SoundtouchPackage (Package):
 		if arch == 'darwin-32':
 			self.ld_flags = ['-arch i386']
 			self.gcc_flags = ['-arch i386']
-			self.configure = 'AM_CPPFLAGS="-arch i386" ./configure --prefix="%{package_prefix}"'
+			self.configure = 'AM_CXXFLAGS="-arch i386" ./configure --prefix="%{package_prefix}"'
 			self.local_configure_flags.extend (['--build=i386-apple-darwin11.2.0'])
 		elif arch == 'darwin-64':
 			self.ld_flags = ['-arch x86_64']
 			self.gcc_flags = ['-arch x86_64']
-			self.configure = 'AM_CPPFLAGS="-arch x86_64" ./configure --prefix="%{package_prefix}"'
+			self.configure = 'AM_CXXFLAGS="-arch x86_64" ./configure --prefix="%{package_prefix}"'
 			self.local_configure_flags.extend (['--build=x86_64-apple-darwin11.2.0'])
 
 		Package.arch_build (self, arch, defaults = False)
