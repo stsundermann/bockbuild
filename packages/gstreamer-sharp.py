af901691973115433c840f455b793e2f6b3fe1de
Stephan Sundermann <stephansundermann@gmail.com>
2015-05-28 16:16:17 +0200

    gstreamer-sharp: Update to latest master to fix 32 bits issues

--- packages/gstreamer-sharp.py
+++ packages/gstreamer-sharp.py
@@ -1,1 +1,1 @@
-FreeDesktopGitPackage ('gstreamer', 'gstreamer-sharp', '1.0', revision = '2bf30fd612a399643521c08e388dfe823b023f4b')
+FreeDesktopGitPackage ('gstreamer', 'gstreamer-sharp', '1.0', revision = '9556b05947a43cbeea57113f427d6bde5246c77f')
