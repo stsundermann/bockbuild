f25ef955eec4857cec75d4a3c1c2c5285cbdcfce
Andrés G. Aragoneses <knocte@gmail.com>
2015-06-15 23:03:47 +0200

    [packages] add --disable-asm configure flag for libgcrypt (bgo#751013)
    As it happens with the macports' package [1], we need this configure
    flag, as without it we could get compilation problems, as evidenced
    in https://bugzilla.gnome.org/show_bug.cgi?id=751013.
    
    [1] https://trac.macports.org/browser/trunk/dports/devel/libgcrypt/Portfile

--- packages/libgcrypt.py
+++ packages/libgcrypt.py
@@ -1,3 +1,8 @@
-Package ('libgcrypt', '1.6.3', sources = [
+class LibGcryptPackage (Package):
+	def __init__ (self):
+		Package.__init__ (self, 'libgcrypt', '1.6.3',
+                                  configure_flags = [ '--disable-asm' ], sources = [
 	'ftp://ftp.gnupg.org/gcrypt/%{name}/%{name}-%{version}.tar.bz2'
-])
+                                  ])
+
+LibGcryptPackage ()
