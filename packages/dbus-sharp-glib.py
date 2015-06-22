acc635ebfe66540e900916e008b1dd195cdfa5fe
Andrés G. Aragoneses <knocte@gmail.com>
2015-06-22 03:16:53 +0200

    [banshee] Go back to stable versions of dbus-sharp* pkgs
    Banshee configure phase would still fail with these new
    versions, let's go back to the previous ones for now.

--- packages/dbus-sharp-glib.py
+++ packages/dbus-sharp-glib.py
@@ -1,11 +1,11 @@
 class DbusSharpGlibPackage (GitHubPackage):
-	def __init__ (self):		
-		GitHubPackage.__init__ (self, 'mono', 'dbus-sharp-glib', '0.5.0.1',
-			revision = 'eeccd9126d050c0537be64530a933adefe2ee5a6')
+	def __init__ (self):
+		GitHubPackage.__init__ (self, 'mono', 'dbus-sharp-glib', '0.5.0',
+			revision = '7a2e676e867fec8db6185334d4242ec440895c27')
 
 	def arch_build (self, arch):
 		self.sh ('sed -ie "s:GMCS, gmcs, no:GMCS, mcs, no:g" "configure.ac"')
 		self.sh ('./autogen.sh --prefix="%{prefix}"')
 		Package.arch_build (self, arch, defaults = False)
- 
+
 DbusSharpGlibPackage ()
--- packages/dbus-sharp.py
+++ packages/dbus-sharp.py
@@ -1,13 +1,13 @@
 class DbusSharpPackage (GitHubPackage):
-	def __init__ (self):		
-		GitHubPackage.__init__ (self, 'mono', 'dbus-sharp', '0.8.99',
-			revision = 'fb3dd1221fb5dceb4c07d7747f638d940680e63f')
+	def __init__ (self):
+		GitHubPackage.__init__ (self, 'mono', 'dbus-sharp', '0.7.0',
+			revision = '60cd041dc9676161ed1dcce0652619cad3128159')
 
 	def arch_build (self, arch):
 
 		# Use mcs rather than gmcs
 		self.sh ('sed -ie "s:GMCS, gmcs, no:GMCS, mcs, no:g" "configure.ac"')
-		
+
 		self.sh ('./autogen.sh --prefix="%{prefix}"')
 		Package.arch_build (self, arch, defaults = False)
 
